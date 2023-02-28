1f84c38e33077214c808abd335b5682a8cf08ee1
Gilmar Moraes <gilmar14moraes@hotmail.com>
2023-02-28 11:33:53 -0300

    Ajuste da classe Endereço repository e inclusão do doc

--- API-Pessoa/src/main/java/com/teste/api/repository/EnderecoRepository.java
+++ API-Pessoa/src/main/java/com/teste/api/repository/EnderecoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.teste.api.model.EnderecoModel;
-import com.teste.api.model.PessoaModel;
 
 @Repository
 public interface EnderecoRepository extends JpaRepository<EnderecoModel, Long>{
